abaa26bc56193b7dba64a0f83e59320f896641e1
Wayra <pierostm2000@hotmail.com>
2014-02-10 15:58:44 -0500

    last commit

--- main.lua
+++ main.lua
@@ -6,15 +6,14 @@ display.setStatusBar( display.HiddenStatusBar )
 
 local physics = require("physics")
 physics.start( )
-
-physics.setDrawMode( "normal" )
+physics.setGravity(0, 0)
+physics.setDrawMode( "hybrid" )
 
 local ancho = display.contentWidth
 local alto = display.contentHeight
 
-local velocity  = 30
-
-
+local velocity  = 40
+local score = 0
 ------------------
 --backgrounds-----------------
 -----------------------------
@@ -45,7 +44,7 @@ derecha.y = alto / 2
 local izquierda = display.newRect( 0, 0, 5, alto )
 izquierda.x = ancho - 5
 izquierda.y = alto / 2
-arriba.isVisible = false
+arriba.isVisible = faldsase
 abajo.isVisible = false
 derecha.isVisible = false
 izquierda.isVisible = false
@@ -61,9 +60,16 @@ physics.addBody( izquierda, "static",{density=0.3, friction=0.3, bounce=0.3} )
 local character = display.newCircle( 0, 0, 50 )
 character.x = ancho - 160
 character.y = alto / 2
-character:setFillColor( 0.5,1,0 )
-physics.addBody( character, {density= 0.3, friction=0.3, bounce=0.4, radius = 60 } )
+character:setFillColor( 1,0.2,0 )
+physics.addBody( character, {density=0.3, friction=0.3, bounce=0.3,radius = 50} )
+character.type = "character"
+character.collision = onCollision
+character:addEventListener( "collision", character )
 
+local scoreText = display.newText("Score: "..score,0,0, native.systemFontBold,60 )
+scoreText.x = ancho / 2
+scoreText.y = alto - 50
+scoreText:setFillColor( 0,1,0 )
 
 --function moverCharacter(event)
 --		physics.setGravity(10 * event.xGravity,-10 * event.yGravity)
@@ -71,23 +77,83 @@ physics.addBody( character, {density= 0.3, friction=0.3, bounce=0.4, radius = 60
 --	return true
 --end
 
+function moveCharacter(event)
+	if event.phase == "began" then
+		character.isFocus = true
+	elseif event.phase == "moved" then
+		eventY = event.y 
+		character.y = eventY 
+	elseif event.phase == "ended" then
+		character.isFocus = false
+	end
+	return true
+end
 
+ 
+function addEnemy(event)
+	local enemy = display.newRect( 0, 0, 70, 70 )
+	enemy.x = 50
+	enemy.y = math.random(10, alto)
+	enemy.name = "enemy"
+	enemy:setFillColor( 0,0,1 )
+	physics.addBody( enemy , {density=0.3, friction=0.3, bounce=0.3 } )
+	enemy.isSensor = true	
+	enemies:insert(enemy)
+end
 
 function loopGame(event)
-	if background1.x >  ancho + 520 then
-		background1.x = -ancho / 2 + velocity * 2 
+	---------------------------------------------------------
+	------------------------Movimiento de Background ------
+	-------------------------------------------------------
+
+	if background1.x >  ancho + ancho / 2 - 50 then
+		background1.x = -ancho / 2 + velocity + 5
 	else
 		background1.x = background1.x + velocity
 	end
 
-	if background2.x > ancho+520 then
-		background2.x = -ancho / 2   + velocity * 2 
+	if background2.x > ancho+ ancho / 2 - 50 then
+		background2.x = -ancho / 2   + velocity + 5
 	else
 		background2.x = background2.x + velocity
 	end
+	---------------------------------------------------------
+	------------------------Fin movimiento de Background ------
 
+	----------------------------------------------------------
+	---------------------------Movimiento de Enemigos-------------
+	----------------------------------------------
+	if enemies.numChildren ~= 0 then
+		 i = 1
+		while i <= enemies.numChildren do 
+			enemies[i].x = enemies[i].x + velocity
+			if enemies[i].x > ancho then
+				enemies:remove(enemies[i])
+				enemies[i] = nil
+				score = score + 1
+				scoreText.text = "Score: "..score
+			end
+		i = i + 1
+		end
+
+	end
+	-------------------------------------------------------
+	-------------------------Control de colisiones----------
+	-----------------------------------------------------
+	
 	return true
 end
 
+function onCollision(self, event)
+	if event.phase == "began" then
+		if event.target.type == "character" and event.other.type == "enemy" then
+		print("choque ")
+		end
+	end
+	return true
+end
+
+timer.performWithDelay( 200, addEnemy, 0 )
+Runtime:addEventListener( "touch", moveCharacter)
 Runtime:addEventListener( "enterFrame", loopGame )
 --Runtime:addEventListener( "accelerometer", moveCharacter )
